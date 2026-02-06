5204d234ca418234fa074f678643bf6fcc2408ba
moredeepa77 <moredeepa77@gmail.com>
2026-02-06 19:37:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-const noButton = document.querySelector(".no-button");
-const yesButton = document.querySelector(".yes-button");
-
 function handleYesClick() {
   window.location.href = "yes_page.html";
 }
 
 function handleNoClick() {
-  const buttonWidth = noButton.offsetWidth;
-  const buttonHeight = noButton.offsetHeight;
-
-  const maxX = window.innerWidth - buttonWidth;
-  const maxY = window.innerHeight - buttonHeight;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noButton.style.position = "absolute";
-  noButton.style.left = `${randomX}px`;
-  noButton.style.top = `${randomY}px`;
+  // No button should stay in place
+  // GIF logic will be added later
+  console.log("No clicked");
 }
-
-// Extra evil mode: also move on hover
-noButton.addEventListener("mouseover", handleNoClick);
